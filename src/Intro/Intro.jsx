286481260ebfaa9d2e63dad95290ffef6154d294
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-06-04 18:44:33 -0400

    feat: Added link to Medium blogs

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -2,6 +2,8 @@ import styles from './Intro.module.css';
 import Github from '/assets/github.svg';
 import LinkedIn from '/assets/linkedIn.svg';
 import Instagram from '/assets/instagram.svg';
+import MediumBlog from '/assets/blog.svg';
+
 import { useState, useEffect } from 'react';
 function Intro(){
     const [isVisible, setIsVisible] = useState(false);
@@ -13,6 +15,7 @@ function Intro(){
                 <a href="https://www.instagram.com/nate_0901/" ><img className={styles.icon} src={Instagram} alt="Instagram Icon"/></a>
                 <a href="https://www.linkedin.com/in/nathaniel-martes/"><img className={styles.icon} src={LinkedIn} alt="LinkedIn Icon"/></a>
                 <a href="https://github.com/NateMartes"><img className={styles.icon} src={Github} alt="Github Icon"/></a>
+                <a href="https://medium.com/@nathaniel.martes.business"><img className={styles.icon} src={MediumBlog} alt="Medium Blog"/></a>
             </div>
         </div>
     )
