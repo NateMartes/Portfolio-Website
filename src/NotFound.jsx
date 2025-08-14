43f8d6e838b1f3a330e2778b4a689c8deeeeadb6
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-08-14 15:45:44 -0400

    fix: Added custom 404 page and updated build process so Github Pages doesn't break

---
+++ src/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      <h2>The Page you are looking for does not exists, or you have a typo, which happens...</h2>
+    </>
+  );
+}
+
+export default NotFound;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import TheSecretOfUs from "./TheSecretOfUs/TheSecretOfUs.jsx"
-import "./index.css"
+import TheSecretOfUs from "./TheSecretOfUs/TheSecretOfUs.jsx";
+import NotFound from "./NotFound.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/thesecretofus" element={<TheSecretOfUs />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
