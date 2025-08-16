2d3de7eb948245a5ec196087c741b348ce3a9d3e
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-08-15 20:01:05 -0400

    fix: Made images smaller

--- src/TheSecretOfUs/TheSecretOfUs.jsx
+++ src/TheSecretOfUs/TheSecretOfUs.jsx
@@ -25,23 +25,23 @@ function TheSecretOfUs() {
   return (
     <>
       <div className={styles.theSecretOfUs}>
-        <h1>If you're reading this, I want you to know that...</h1>
+        <h2>If you're reading this, I want you to know that...</h2>
         <img className={styles.theSecretOfUsDownArrow} src={downArrow} type="svg"/>
       </div>
       <div className={styles.theSecretOfUs}>
-        <img src={catHeart} height="400px" width="400px" />
-        <h1>I LOVE YOU!!! And I'll use this whole page to tell you how much I love you!</h1>
+        <img src={catHeart} height="250px" width="250px" />
+        <h2>I LOVE YOU!!! And I'll use this whole page to tell you how much I love you!</h2>
         <img className={styles.theSecretOfUsDownArrow} src={downArrow} type="svg"/>
       </div>
       <div className={styles.theSecretOfUs}>
-        <img src={catNerd} height="400px" width="400px" />
-        <h1>I love you more than the digits in PI, which there are over 100 trillon according to Google.
-        Google has a way for me to get all the digits but your phone would explode so just trust me! :)</h1>
+        <img src={catNerd} height="250px" width="250px" />
+        <h2>I love you more than the digits in PI, which there are over 100 trillon according to Google.
+        Google has a way for me to get all the digits but your phone would explode so just trust me! :)</h2>
         <img className={styles.theSecretOfUsDownArrow} src={downArrow} type="svg"/>
       </div>
       <div className={styles.theSecretOfUs}>
-        <img src={cinnamaroll} height="400px" width="400px" />
-        <h1>I love you because you smell so amazing, just like this wild Cinamaroll.</h1>
+        <img src={cinnamaroll} height="250px" width="250px" />
+        <h2>I love you because you smell so amazing, just like this wild Cinamaroll.</h2>
         <img className={styles.theSecretOfUsDownArrow} src={downArrow} type="svg"/>
       </div>
     </>
